35c7e5da1c7eb94e30bae8506cc6d56c9dbf2c53
pexcn <pexcn97@gmail.com>
2016-09-04 01:00:05 +0800

    Update init.qcom.post_boot.sh
    Change-Id: I4a01e8f25f11e08ccbcf9f9b9ca21d841dbcbf1f

--- init.qcom.post_boot.sh
+++ init.qcom.post_boot.sh
@@ -15,4 +15,17 @@ echo 0 > /sys/block/mmcblk0/queue/add_random
 echo 0 > /sys/block/mmcblk0/queue/iostats
 echo 0 > /sys/block/mmcblk0rpmb/queue/add_random
 echo 0 > /sys/block/mmcblk0rpmb/queue/iostats
+echo 0 > /sys/module/ipc_router/parameters/debug_mask
+echo 0 > /sys/module/alarm/parameters/debug_mask
+echo 0 > /sys/module/smp2p/parameters/debug_mask
+echo 0 > /sys/module/msm_serial_hs/parameters/debug_mask
+echo 0 > /sys/module/msm_bt_sleep/parameters/debug_mask
+echo 0 > /sys/module/alarm_dev/parameters/debug_mask
+echo 0 > /sys/module/touch_synaptics/parameters/debug_mask
+echo 0 > /sys/module/mpm_of/parameters/debug_mask
+echo 0 > /sys/module/pm_8x60/parameters/debug_mask
+echo 0 > /sys/module/touch_synaptics_ds5/parameters/debug_mask
+echo 0 > /sys/module/lowmemorykiller/parameters/debug_level
+echo 0 > /sys/kernel/debug/msm_vidc/debug_level
+echo 0 > /sys/kernel/debug/msm_vidc/fw_debug_mode
 
