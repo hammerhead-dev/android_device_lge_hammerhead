733064cc8400f912b09eec1ca5be04fd4cbb4c10
pexcn <pexcn97@gmail.com>
2016-09-14 16:13:34 +0800

    Follow kernel updates
    Change-Id: I5b3bcd8c7ecac4af854f237598ee6fb6a88523b3
    
    Clean file_contexts
    
    Change-Id: Iadfa0c6b9b585ba4e8253f9585ec7378f9a30cfe
    
    Update
    
    Change-Id: Ic4f6129ee5f1852b19bd12002227a6595035c221
    
    Update init.qcom.post_boot.sh
    
    Change-Id: Ib21e4226d586009812194ee0daf45cb5d4e474c1

--- init.qcom.post_boot.sh
+++ init.qcom.post_boot.sh
@@ -1,7 +1,5 @@
 #!/system/bin/sh
 
-# 300000 422400 652800 729600 883200 960000 1036800 1190400 1267200 1497600 1574400 1728000 1958400 2265600
-
 echo 300000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 echo 300000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
 echo 300000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq
@@ -10,17 +8,13 @@ echo 2265600 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 echo 2265600 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
 echo 2265600 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
 echo 2265600 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
-chmod 444 /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-chmod 444 /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
-chmod 444 /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq
-chmod 444 /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq
-chmod 444 /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
-chmod 444 /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
-chmod 444 /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
-chmod 444 /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
+
 echo 1 > /sys/devices/system/cpu/sched_mc_power_savings
+echo "750 750 750 760 780 790 800 820 835 870 885 915 960 1020" > /sys/devices/system/cpu/cpu0/cpufreq/UV_mV_table
+
 echo 0 > /sys/block/mmcblk0/queue/iostats
 echo 0 > /sys/block/mmcblk0rpmb/queue/iostats
+
 echo 0 > /sys/module/ipc_router/parameters/debug_mask
 echo 0 > /sys/module/alarm/parameters/debug_mask
 echo 0 > /sys/module/smp2p/parameters/debug_mask
@@ -32,6 +26,5 @@ echo 0 > /sys/module/mpm_of/parameters/debug_mask
 echo 0 > /sys/module/pm_8x60/parameters/debug_mask
 echo 0 > /sys/module/touch_synaptics_ds5/parameters/debug_mask
 echo 0 > /sys/module/lowmemorykiller/parameters/debug_level
-echo 0 > /sys/kernel/debug/msm_vidc/debug_level
 echo 0 > /sys/kernel/debug/msm_vidc/fw_debug_mode
 
